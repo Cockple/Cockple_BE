152f113f0b3f27ef94fd86149efd6c2b8a71704b
dbalsk <dbalsrltk@naver.com>
2025-07-04 22:47:28 +0900

    feat: 응답 DTO 클래스 구현

--- src/main/java/umc/cockple/demo/global/response/dto/ReasonDTO.java
+++ src/main/java/umc/cockple/demo/global/response/dto/ReasonDTO.java
@@ -1,4 +1,21 @@
 package umc.cockple.demo.global.response.dto;
 
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Builder
 public class ReasonDTO {
+    private final String code;
+    private final String message;
+    private final HttpStatus httpStatus;
+
+    public static ReasonDTO of(String code, String message, HttpStatus httpStatus) {
+        return ReasonDTO.builder()
+                .code(code)
+                .message(message)
+                .httpStatus(httpStatus)
+                .build();
+    }
 }
