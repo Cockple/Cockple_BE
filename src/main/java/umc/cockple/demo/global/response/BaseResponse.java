3aef26f27d4570d63be0785eaa288dc48f8e8a83
dbalsk <dbalsrltk@naver.com>
2025-07-04 23:35:50 +0900

    feat: API 공통 응답 성공 파트 구현

--- src/main/java/umc/cockple/demo/global/response/BaseResponse.java
+++ src/main/java/umc/cockple/demo/global/response/BaseResponse.java
@@ -3,8 +3,11 @@ package umc.cockple.demo.global.response;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Getter;
+import umc.cockple.demo.global.response.code.BaseCode;
 import umc.cockple.demo.global.response.code.BaseErrorCode;
+import umc.cockple.demo.global.response.code.status.CommonSuccessCode;
 import umc.cockple.demo.global.response.dto.ErrorReasonDTO;
+import umc.cockple.demo.global.response.dto.ReasonDTO;
 
 @Getter
 @Builder
@@ -17,6 +20,49 @@ public class BaseResponse<T> {
     private final T data;
     private final ErrorReasonDTO errorReason;
 
+    //성공 응답 생성: 결과 데이터 포함
+    public static <T> BaseResponse<T> success(BaseCode successCode, T data) {
+        ReasonDTO reason = successCode.getReason();
+        return BaseResponse.<T>builder()
+                .isSuccess(true)
+                .code(reason.getCode())
+                .message(reason.getMessage())
+                .data(data)
+                .build();
+    }
+
+    //성공 응답 생성: 결과 데이터 없음
+    public static <T> BaseResponse<T> success(BaseCode successCode) {
+        ReasonDTO reason = successCode.getReason();
+        return BaseResponse.<T>builder()
+                .isSuccess(true)
+                .code(reason.getCode())
+                .message(reason.getMessage())
+                .build();
+    }
+
+    //성공 응답 생성: 결과 데이터 포함, 커스텀 메시지 포함
+    public static <T> BaseResponse<T> success(BaseCode successCode, String customMessage, T data) {
+        ReasonDTO reason = successCode.getReason();
+        return BaseResponse.<T>builder()
+                .isSuccess(true)
+                .code(reason.getCode())
+                .message(customMessage)
+                .data(data)
+                .build();
+    }
+
+    //성공 응답 생성: 결과 데이터 없음, 커스텀 메시지 포함
+    public static <T> BaseResponse<T> success(BaseCode successCode, String customMessage) {
+        ReasonDTO reason = successCode.getReason();
+        return BaseResponse.<T>builder()
+                .isSuccess(true)
+                .code(reason.getCode())
+                .message(customMessage)
+                .build();
+    }
+
+    //실패 응답 생성
     public static <T> BaseResponse<T> error(BaseErrorCode errorCode) {
         ErrorReasonDTO errorReason = errorCode.getReason();
         return BaseResponse.<T>builder()
@@ -27,6 +73,7 @@ public class BaseResponse<T> {
                 .build();
     }
 
+    //실패 응답 생성: 커스텀 메시지 포함
     public static <T> BaseResponse<T> error(BaseErrorCode errorCode, String customMessage) {
         ErrorReasonDTO errorReason = errorCode.getReason();
         return BaseResponse.<T>builder()
