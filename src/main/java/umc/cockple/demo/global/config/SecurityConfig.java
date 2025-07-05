5fdf2918cab9d3c8828900022668ca6f351b7ea4
rim <kkjjrr0504@gmail.com>
2025-07-05 17:22:26 +0900

    feat: 스웨거 설정

---
+++ src/main/java/umc/cockple/demo/global/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package umc.cockple.demo.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(); // 또는 formLogin().disable() 하면 Swagger만 테스트 가능
+
+        return http.build();
+    }
+}
