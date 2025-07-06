59a2ce068f7190405d74efb8e649ca20b99b045a
kanghana1 <qazplm12093@naver.com>
2025-07-07 01:29:36 +0900

    feat: 스프링 시큐리티 Config 파일 추가

--- src/main/java/umc/cockple/demo/global/config/SecurityConfig.java
+++ src/main/java/umc/cockple/demo/global/config/SecurityConfig.java
@@ -1,26 +1,50 @@
 package umc.cockple.demo.global.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    @Bean
+    public WebSecurityCustomizer configure() {
+        return web -> web.ignoring().requestMatchers(
+                "/v3/api-docs/**",
+                "/swagger-ui/**",
+                "/swagger-ui.html",
+                "/swagger-resources/**",
+                "/configuration/**",
+                "/webjars/**",
+                "/favicon.ico"
+        );
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
-                                "/v3/api-docs/**",
-                                "/swagger-ui/**",
-                                "/swagger-ui.html"
+                                "/api/member/**", "/",
+                                "/api/terms", "/api/nickname"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
-                .formLogin(); // 또는 formLogin().disable() 하면 Swagger만 테스트 가능
+                .formLogin(AbstractHttpConfigurer::disable)
+//                .addFilterBefore() // 추후 jwt 개발시 사용
+        ;
 
         return http.build();
     }
