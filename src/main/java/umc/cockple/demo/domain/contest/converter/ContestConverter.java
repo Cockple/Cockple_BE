043373931f5c5602ddbcc705cf18607ff5c819bf
rim <kkjjrr0504@gmail.com>
2025-07-10 15:49:36 +0900

    feat: converter 추가 구현

--- src/main/java/umc/cockple/demo/domain/contest/converter/ContestConverter.java
+++ src/main/java/umc/cockple/demo/domain/contest/converter/ContestConverter.java
@@ -2,14 +2,18 @@ package umc.cockple.demo.domain.contest.converter;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import umc.cockple.demo.domain.contest.domain.Contest;
 import umc.cockple.demo.domain.contest.dto.ContestRecordCreateCommand;
 import umc.cockple.demo.domain.contest.dto.ContestRecordCreateRequestDTO;
+import umc.cockple.demo.domain.contest.dto.ContestRecordCreateResponseDTO;
+import umc.cockple.demo.domain.exercise.domain.Exercise;
+import umc.cockple.demo.domain.exercise.dto.ExerciseCreateResponseDTO;
 
 @Component
 @RequiredArgsConstructor
 public class ContestConverter {
 
-    public static ContestRecordCreateCommand toCreateCommand(ContestRecordCreateRequestDTO request, Long memberId) {
+    public ContestRecordCreateCommand toCreateCommand(ContestRecordCreateRequestDTO request, Long memberId) {
         return ContestRecordCreateCommand.builder()
                 .memberId(memberId)
                 .contestName(request.contestName())
@@ -22,6 +26,13 @@ public class ContestConverter {
                 .videoIsOpen(request.videoIsOpen())
                 .contestVideos(request.contestVideos())
                 .contestImgs(request.contestImgs())
+                .build();
+    }
+
+    public ContestRecordCreateResponseDTO toCreateResponseDTO(Contest contest) {
+        return ContestRecordCreateResponseDTO.builder()
+                .contestId(contest.getId())
+                .createdAt(contest.getCreatedAt())
                 .build();
     }
 }
