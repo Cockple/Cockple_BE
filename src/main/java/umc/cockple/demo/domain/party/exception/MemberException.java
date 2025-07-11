b84d4800014f25b68524c8438b1d9baa60a7576a
dbalsk <dbalsrltk@naver.com>
2025-07-12 00:32:50 +0900

    refactor: merge로 인한 코드 중복생성 삭제

--- src/main/java/umc/cockple/demo/domain/party/exception/MemberException.java
+++
@@ -1,9 +0,0 @@
-package umc.cockple.demo.domain.party.exception;
-
-import umc.cockple.demo.global.exception.GeneralException;
-import umc.cockple.demo.global.response.code.BaseErrorCode;
-
-public class MemberException extends GeneralException {
-
-    public MemberException(BaseErrorCode code) { super(code); }
-}
