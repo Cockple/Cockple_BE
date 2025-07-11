15b818616fec0436c1832738abd8b96a93a7d448
rim <kkjjrr0504@gmail.com>
2025-07-11 14:12:43 +0900

    feat: 대회 기록 등록 서비스 구현

--- src/main/java/umc/cockple/demo/domain/contest/service/ContestCommandServiceImpl.java
+++ src/main/java/umc/cockple/demo/domain/contest/service/ContestCommandServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import umc.cockple.demo.domain.contest.converter.ContestConverter;
 import umc.cockple.demo.domain.contest.domain.Contest;
+import umc.cockple.demo.domain.contest.domain.ContestImg;
 import umc.cockple.demo.domain.contest.domain.ContestVideo;
 import umc.cockple.demo.domain.contest.dto.ContestRecordCreateCommand;
 import umc.cockple.demo.domain.contest.dto.ContestRecordCreateRequestDTO;
@@ -17,6 +18,7 @@ import umc.cockple.demo.domain.member.repository.MemberRepository;
 import umc.cockple.demo.global.s3.ImageService;
 
 import java.util.List;
+import java.util.UUID;
 
 @Slf4j
 @Service
@@ -49,7 +51,7 @@ public class ContestCommandServiceImpl implements ContestCommandService {
                 .orElseThrow(() -> new RuntimeException("회원이 존재하지 않습니다"));
 
         //2. DTO -> Command
-        ContestRecordCreateCommand contestRecordCommand = contestConverter.toCreateCommand(request, memberId);
+        ContestRecordCreateCommand contestRecordCommand = contestConverter.toCreateCommand(request, memberId, contestImgs);
 
         log.info("받은 공개 여부 - content: {}, video: {}", request.contentIsOpen(), request.videoIsOpen());
 
@@ -60,6 +62,21 @@ public class ContestCommandServiceImpl implements ContestCommandService {
         // 4. 이미지 업로드 -> ContestImg로 변환
         if (contestRecordCommand.contestImgs() != null) {
             List<String> imageUrls = imageService.uploadImages(contestRecordCommand.contestImgs());
+
+            for (int i = 0; i < imageUrls.size(); i++) {
+                String imgUrl = imageUrls.get(i);
+                String uniqueKey = UUID.randomUUID().toString(); // 나중에 파일명 대체 가능
+
+
+                ContestImg contestImg = ContestImg.builder()
+                        .contest(contest)
+                        .imgUrl(imgUrl)
+                        .imgKey(uniqueKey) // 임시 키
+                        .imgOrder(i)
+                        .build();
+
+                contest.addContestImg(contestImg);
+            }
         }
 
         // 5. 영상 URL -> ContestVideo로 변환
