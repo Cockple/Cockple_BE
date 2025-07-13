d8c4baeea95a002ae25679ab8d93b370b9e0837a
dmori <chosehoon2@naver.com>
2025-07-13 17:39:07 +0900

    feat: requestDTO 파싱 로직 구현

--- src/main/java/umc/cockple/demo/domain/exercise/dto/GuestInviteRequestDTO.java
+++ src/main/java/umc/cockple/demo/domain/exercise/dto/GuestInviteRequestDTO.java
@@ -3,6 +3,10 @@ package umc.cockple.demo.domain.exercise.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import umc.cockple.demo.domain.exercise.exception.ExerciseErrorCode;
+import umc.cockple.demo.domain.exercise.exception.ExerciseException;
+import umc.cockple.demo.global.enums.Gender;
+import umc.cockple.demo.global.enums.Level;
 
 public record GuestInviteRequestDTO(
         @NotBlank(message = "게스트 이름은 필수입니다.")
@@ -19,4 +23,27 @@ public record GuestInviteRequestDTO(
                 message = "올바른 급수를 입력해주세요.")
         String level
 ) {
+        public Gender toParsedGender() {
+                return switch (gender) {
+                        case "남성" -> Gender.MALE;
+                        case "여성" -> Gender.FEMALE;
+                        default -> throw new ExerciseException(ExerciseErrorCode.INVALID_GENDER_FORMAT);
+                };
+        }
+
+        public Level toParsedLevel() {
+                String normalizedLevel = level.trim().toUpperCase();
+
+                return switch (normalizedLevel) {
+                        case "자강" -> Level.EXPERT;
+                        case "준자강" -> Level.SEMI_EXPERT;
+                        case "A조" -> Level.A;
+                        case "B조" -> Level.B;
+                        case "C조" -> Level.C;
+                        case "D조" -> Level.D;
+                        case "초심" -> Level.BEGINNER;
+                        case "왕초심" -> Level.NOVICE;
+                        case "급수없음" -> Level.NONE;
+                        default -> throw new ExerciseException(ExerciseErrorCode.INVALID_LEVEL_FORMAT);
+                };
 }
