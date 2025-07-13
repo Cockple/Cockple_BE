27d41a6ff4cb554fd0a6ab96f2c6cde3e5b33b05
rim <kkjjrr0504@gmail.com>
2025-07-13 15:46:10 +0900

    fix: 사용 안해서 삭제함

--- src/main/java/umc/cockple/demo/domain/contest/repository/ContestImgRepository.java
+++
@@ -1,7 +0,0 @@
-package umc.cockple.demo.domain.contest.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import umc.cockple.demo.domain.contest.domain.ContestImg;
-
-public interface ContestImgRepository extends JpaRepository<ContestImg,Long> {
-}
--- src/main/java/umc/cockple/demo/domain/contest/repository/ContestVideoRepository.java
+++
@@ -1,7 +0,0 @@
-package umc.cockple.demo.domain.contest.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import umc.cockple.demo.domain.contest.domain.ContestVideo;
-
-public interface ContestVideoRepository extends JpaRepository<ContestVideo,Long> {
-}
