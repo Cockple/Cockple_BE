498aa563812335072f9b84f7551ed04e37ad5f1c
dbalsk <dbalsrltk@naver.com>
2025-07-16 00:19:16 +0900

    feat: 모임 서비스 인터페이스에 가입신청 처리 메서드 추가

--- src/main/java/umc/cockple/demo/domain/party/service/PartyCommandService.java
+++ src/main/java/umc/cockple/demo/domain/party/service/PartyCommandService.java
@@ -3,11 +3,15 @@ package umc.cockple.demo.domain.party.service;
 import org.springframework.web.multipart.MultipartFile;
 import umc.cockple.demo.domain.party.dto.PartyCreateRequestDTO;
 import umc.cockple.demo.domain.party.dto.PartyCreateResponseDTO;
+import umc.cockple.demo.domain.party.dto.PartyJoinActionRequestDTO;
 import umc.cockple.demo.domain.party.dto.PartyJoinCreateResponseDTO;
+import umc.cockple.demo.global.response.BaseResponse;
 
 public interface PartyCommandService {
 
     PartyCreateResponseDTO createParty(Long memberId, MultipartFile profileImage, PartyCreateRequestDTO request);
 
     PartyJoinCreateResponseDTO createJoinRequest(Long partyId, Long memberId);
+
+    void actionJoinRequest(Long partyId, Long memberId, PartyJoinActionRequestDTO request, Long requestId);
 }
