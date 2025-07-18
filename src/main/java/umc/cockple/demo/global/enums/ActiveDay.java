1b3c16fcff2e39192f2d791dd3f9348d82ee33eb
dbalsk <dbalsrltk@naver.com>
2025-07-19 04:15:33 +0900

    feat: ENUM에 한글 추가 및 getter 추가

--- src/main/java/umc/cockple/demo/global/enums/ActiveDay.java
+++ src/main/java/umc/cockple/demo/global/enums/ActiveDay.java
@@ -1,12 +1,22 @@
 package umc.cockple.demo.global.enums;
 
+import lombok.Getter;
+
 public enum ActiveDay {
 
-    SUNDAY,
-    MONDAY,
-    TUESDAY,
-    WEDNESDAY,
-    THURSDAY,
-    FRIDAY,
-    SATURDAY
+    SUNDAY("일"),
+    MONDAY("월"),
+    TUESDAY("화"),
+    WEDNESDAY("수"),
+    THURSDAY("목"),
+    FRIDAY("금"),
+    SATURDAY("토");
+
+    @Getter
+    private final String koreanName;
+
+    ActiveDay(String koreanName) {
+        this.koreanName = koreanName;
+    }
+
 }
--- src/main/java/umc/cockple/demo/global/enums/ActivityTime.java
+++ src/main/java/umc/cockple/demo/global/enums/ActivityTime.java
@@ -1,9 +1,18 @@
 package umc.cockple.demo.global.enums;
 
+import lombok.Getter;
+
 public enum ActivityTime {
 
-    MORNING,
-    AFTERNOON,
-    ALWAYS
+    MORNING("오전"),
+    AFTERNOON("오후"),
+    ALWAYS("상시");
+
+    @Getter
+    private final String koreanName;
+
+    ActivityTime(String koreanName) {
+        this.koreanName = koreanName;
+    }
 
 }
