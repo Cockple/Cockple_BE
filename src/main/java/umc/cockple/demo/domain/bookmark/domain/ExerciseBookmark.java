1439d1e3aa1990d6114fb2eedd9d93c3c81ea80f
dmori <chosehoon2@naver.com>
2025-07-18 23:47:53 +0900

    refactor: 운동 정렬 enum 제거 및 필드 제거

--- src/main/java/umc/cockple/demo/domain/bookmark/domain/ExerciseBookmark.java
+++ src/main/java/umc/cockple/demo/domain/bookmark/domain/ExerciseBookmark.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import lombok.*;
 import umc.cockple.demo.domain.exercise.domain.Exercise;
 import umc.cockple.demo.domain.member.domain.Member;
-import umc.cockple.demo.global.enums.ExerciseOrderType;
 import umc.cockple.demo.global.common.BaseEntity;
 
 @Entity
@@ -25,8 +24,4 @@ public class ExerciseBookmark extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "exercise_id")
     private Exercise exercise;
-
-    @Column(nullable = false)
-    @Enumerated(EnumType.STRING)
-    private ExerciseOrderType orderType;
 }
