c208c146bc08e39bdb517780e8410fa20d3068ce
dmori <chosehoon2@naver.com>
2025-07-19 18:22:44 +0900

    feat: 게스트 번호와 참여여부 record DTO로 이동 및 통계 record 추가

--- src/main/java/umc/cockple/demo/domain/exercise/dto/ExerciseMyGuestListDTO.java
+++ src/main/java/umc/cockple/demo/domain/exercise/dto/ExerciseMyGuestListDTO.java
@@ -28,4 +28,36 @@ public class ExerciseMyGuestListDTO {
             String inviterName
     ) {
     }
+
+    @Builder
+    public record GuestGroups(
+            Integer participantNumber,
+            Boolean isWaiting
+    ){
+        public static GuestGroups participant(int number) {
+            return new GuestGroups(number, false);
+        }
+
+        public static GuestGroups waiting(int number) {
+            return new GuestGroups(number, true);
+        }
+    }
+
+    @Builder
+    public record GuestStatistics(
+            int totalCount,     // 전체 게스트 수
+            int maleCount,      // 남자 게스트 수
+            int femaleCount     // 여자 게스트 수
+    ) {
+
+        public static GuestStatistics empty() {
+            return new GuestStatistics(0, 0, 0);
+        }
+
+        public static GuestStatistics of(int totalCount, int maleCount, int femaleCount) {
+            return new GuestStatistics(totalCount, maleCount, femaleCount);
+        }
+    }
+
+
 }
