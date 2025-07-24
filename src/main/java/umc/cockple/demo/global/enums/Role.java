b3d8c37216a47ac4fe271cda2d497e8a0648fe1c
dbalsk <dbalsrltk@naver.com>
2025-07-25 02:26:11 +0900

    refactor: 부모임장 ENUM에 추가

--- src/main/java/umc/cockple/demo/global/enums/Role.java
+++ src/main/java/umc/cockple/demo/global/enums/Role.java
@@ -2,7 +2,9 @@ package umc.cockple.demo.global.enums;
 
 public enum Role {
 
-    party_MEMBER,
+    party_MEMBER, //일반 멤버
 
-    party_MANAGER
+    party_MANAGER, //모임장
+
+    party_SUBMANAGER //부모임장
 }
