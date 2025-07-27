c3e8d5a66e0e9239a4184e1c09fef9024b56bc06
dmori <chosehoon2@naver.com>
2025-07-27 22:43:53 +0900

    feat: DTO 구현

--- src/main/java/umc/cockple/demo/domain/exercise/dto/ExerciseRecommendationDTO.java
+++ src/main/java/umc/cockple/demo/domain/exercise/dto/ExerciseRecommendationDTO.java
@@ -1,4 +1,21 @@
 package umc.cockple.demo.domain.exercise.dto;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class ExerciseRecommendationDTO {
+
+    public record Resposne(
+            Long exerciseId,
+            Long partyId,
+            String partyName,
+            LocalDate date,
+            String dayOfWeek,
+            LocalTime startTime,
+            LocalTime endTime,
+            String buildingName,
+            String imageUrl,
+            Boolean isBookmarked
+    ) {
+    }
 }
