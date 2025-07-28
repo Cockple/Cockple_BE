a41bf692a051826ca5ac6daab2313d69dfa21cc2
dmori <chosehoon2@naver.com>
2025-07-28 18:51:58 +0900

    refactor: 모임 신청 관련 Enum들을 Party 도메인 아래로 변경

--- src/main/java/umc/cockple/demo/domain/party/service/PartyQueryServiceImpl.java
+++ src/main/java/umc/cockple/demo/domain/party/service/PartyQueryServiceImpl.java
@@ -22,6 +22,7 @@ import umc.cockple.demo.domain.party.domain.PartyJoinRequest;
 import umc.cockple.demo.domain.party.domain.PartyKeyword;
 import umc.cockple.demo.domain.party.dto.*;
 import umc.cockple.demo.domain.party.enums.PartyStatus;
+import umc.cockple.demo.domain.party.enums.RequestStatus;
 import umc.cockple.demo.domain.party.exception.PartyErrorCode;
 import umc.cockple.demo.domain.party.exception.PartyException;
 import umc.cockple.demo.domain.party.repository.PartyJoinRequestRepository;
