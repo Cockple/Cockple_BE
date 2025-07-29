12161fc66247d5a6607cd3f3e7aeee66beff9a34
Jaerim <kkjjrr0504@gmail.com>
2025-07-29 17:30:06 +0900

    [feat/#198] 채팅 도메인 구현 (#203)

---
+++ src/main/java/umc/cockple/demo/domain/chat/domain/ChatRoom.java
@@ -0,0 +1,39 @@
+package umc.cockple.demo.domain.chat.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import umc.cockple.demo.domain.chat.enums.ChatRoomType;
+import umc.cockple.demo.domain.party.domain.Party;
+import umc.cockple.demo.global.common.BaseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class ChatRoom extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "party_id")
+    private Party party;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private ChatRoomType type;
+
+    @Column(nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "chatRoom", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ChatRoomMember> chatRoomMembers = new ArrayList<>();
+
+    @OneToMany(mappedBy = "chatRoom", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ChatMessage> chatMessages = new ArrayList<>();
+}
