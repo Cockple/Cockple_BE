4c42c0a03ed2eb9aa645166909a4220bf8a4785f
rim <kkjjrr0504@gmail.com>
2025-07-29 15:21:56 +0900

    feat: 채팅 도메인 구현

---
+++ src/main/java/umc/cockple/demo/domain/chat/domain/ChatRoomMember.java
@@ -0,0 +1,27 @@
+package umc.cockple.demo.domain.chat.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import umc.cockple.demo.domain.member.domain.Member;
+import umc.cockple.demo.global.common.BaseEntity;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class ChatRoomMember extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chat_room_id")
+    private ChatRoom chatRoom;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+}
