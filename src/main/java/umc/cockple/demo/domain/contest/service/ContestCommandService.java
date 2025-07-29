4208ca2bbb8fee03da2eb646d0c9f21df386899d
Jaerim <kkjjrr0504@gmail.com>
2025-07-29 15:37:43 +0900

    [refactor/#199] 대회 기록 수정 API에서 이미지 업로드 분리 (#200)
    * refactor: 대회 기록 수정 API 에서 이미지 업로드 분리
    
    * refactor: 대회 기록 수정 API 에서 이미지 업로드 분리
    
    * refactor: 대회 기록 수정 API 에서 이미지 업로드 분리 및 메서드 통합
    
    * fix: 안 쓰는 코드 삭제

--- src/main/java/umc/cockple/demo/domain/contest/service/ContestCommandService.java
+++ src/main/java/umc/cockple/demo/domain/contest/service/ContestCommandService.java
@@ -1,9 +1,6 @@
 package umc.cockple.demo.domain.contest.service;
 
-import org.springframework.web.multipart.MultipartFile;
 import umc.cockple.demo.domain.contest.dto.*;
-
-import java.util.List;
 
 public interface ContestCommandService {
     ContestRecordCreateDTO.Response createContestRecord(
@@ -11,7 +8,7 @@ public interface ContestCommandService {
     );
 
     ContestRecordUpdateDTO.Response updateContestRecord(
-            Long memberId, Long contestId, List<MultipartFile> ContestImage, ContestRecordUpdateDTO.Request request
+            Long memberId, Long contestId, ContestRecordUpdateDTO.Request request
     );
 
     ContestRecordDeleteDTO.Response deleteContestRecord(
