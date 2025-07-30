62316b0b1a1ce60ab65d87c14049ea11b018d66e
rim <kkjjrr0504@gmail.com>
2025-07-30 23:49:21 +0900

    feat: 모임 채팅방 목록 조회 레포지토리 구현

---
+++ src/main/java/umc/cockple/demo/domain/chat/repository/ChatRoomMemberRepository.java
@@ -0,0 +1,24 @@
+package umc.cockple.demo.domain.chat.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import umc.cockple.demo.domain.chat.domain.ChatRoomMember;
+
+import java.util.Optional;
+
+public interface ChatRoomMemberRepository extends JpaRepository<ChatRoomMember, Long> {
+
+    // 채팅방 내 참여자 수
+    @Query("SELECT COUNT(c) FROM ChatRoomMember c WHERE c.chatRoom.id = :chatRoomId")
+    int countByChatRoomId(@Param("chatRoomId") Long chatRoomId);
+
+    // 특정 채팅방에 참여한 특정 멤버 조회
+    @Query("""
+        SELECT c FROM ChatRoomMember c
+        WHERE c.chatRoom.id = :chatRoomId AND c.member.id = :memberId
+    """)
+    Optional<ChatRoomMember> findByChatRoomIdAndMemberId(@Param("chatRoomId") Long chatRoomId,
+                                                         @Param("memberId") Long memberId);
+}
+
