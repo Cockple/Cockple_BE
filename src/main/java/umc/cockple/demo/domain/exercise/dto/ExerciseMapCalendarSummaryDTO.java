2459d75c61e71fb6b054a104685b90d3ef3e7f28
dmori <chosehoon2@naver.com>
2025-07-30 15:21:26 +0900

    feat: DTO 구현

---
+++ src/main/java/umc/cockple/demo/domain/exercise/dto/ExerciseMapCalendarSummaryDTO.java
@@ -0,0 +1,28 @@
+package umc.cockple.demo.domain.exercise.dto;
+
+import lombok.Builder;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+
+public class ExerciseMapCalendarSummaryDTO {
+
+    public record Response(
+            Integer year,
+            Integer month,
+            Double centerLatitude,
+            Double centerLongitude,
+            Double radiusKm,
+            Map<LocalDate, List<BuildingSummary>> buildings) {
+    }
+
+    @Builder
+    public record BuildingSummary(
+            String buildingName,
+            String streetAddr,
+            Double latitude,
+            Double longitude
+    ) {
+    }
+}
