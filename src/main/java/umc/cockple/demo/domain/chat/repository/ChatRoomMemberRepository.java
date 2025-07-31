62f06471bb982f907a439cbbab08e19a57c6dab6
rim <kkjjrr0504@gmail.com>
2025-07-31 21:30:59 +0900

    fix: 안 쓰는 임포트 제거

--- src/main/java/umc/cockple/demo/domain/chat/repository/ChatRoomMemberRepository.java
+++ src/main/java/umc/cockple/demo/domain/chat/repository/ChatRoomMemberRepository.java
@@ -1,13 +1,10 @@
 package umc.cockple.demo.domain.chat.repository;
 
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import umc.cockple.demo.domain.chat.domain.ChatRoom;
 import umc.cockple.demo.domain.chat.domain.ChatRoomMember;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface ChatRoomMemberRepository extends JpaRepository<ChatRoomMember, Long> {
