c6dfe8672521036f0c4f6986dd040b1947743240
dbalsk <dbalsrltk@naver.com>
2025-07-31 20:14:05 +0900

    fix: SecurityConfig 파일 원복

--- src/main/java/umc/cockple/demo/global/config/SecurityConfig.java
+++ src/main/java/umc/cockple/demo/global/config/SecurityConfig.java
@@ -51,9 +51,8 @@ public class SecurityConfig {
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                                .anyRequest().permitAll()
-                        //.requestMatchers("/api/oauth/login").permitAll()
-                        //.anyRequest().authenticated()
+                        .requestMatchers("/api/oauth/login").permitAll()
+                        .anyRequest().authenticated()
                 )
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
                         UsernamePasswordAuthenticationFilter.class)
