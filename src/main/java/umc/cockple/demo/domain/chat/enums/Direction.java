4d53965d7a4c91f453ec2025c2a7bfede3b753f0
rim <kkjjrr0504@gmail.com>
2025-07-31 14:25:25 +0900

    refactor: getter 사용

--- src/main/java/umc/cockple/demo/domain/chat/enums/Direction.java
+++ src/main/java/umc/cockple/demo/domain/chat/enums/Direction.java
@@ -1,16 +1,16 @@
 package umc.cockple.demo.domain.chat.enums;
 
+import lombok.Getter;
+
 public enum Direction {
     DECS("내림차순"),
     ACS("오름차순");
 
+    @Getter
     private final String description;
 
     Direction(String description) {
         this.description = description;
     }
 
-    public String getDescription() {
-        return description;
-    }
 }
