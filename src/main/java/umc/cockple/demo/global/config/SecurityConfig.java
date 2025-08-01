b8f504235419e18e33301eec6905515312e6dbd0
kanghana1 <qazplm12093@naver.com>
2025-08-01 22:51:14 +0900

    fix: Security Config 원상복구

--- src/main/java/umc/cockple/demo/global/config/SecurityConfig.java
+++ src/main/java/umc/cockple/demo/global/config/SecurityConfig.java
@@ -51,9 +51,8 @@ public class SecurityConfig {
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-//                        .requestMatchers("/api/oauth/login").permitAll()
-//                        .anyRequest().authenticated()
-                                .anyRequest().permitAll()
+                        .requestMatchers("/api/oauth/login").permitAll()
+                        .anyRequest().authenticated()
                 )
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
                         UsernamePasswordAuthenticationFilter.class)
