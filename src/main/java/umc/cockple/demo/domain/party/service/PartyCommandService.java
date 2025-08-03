511df81273323df580f25663543dd76d0cddba2b
dbalsk <dbalsrltk@naver.com>
2025-08-04 01:13:33 +0900

    feat: 멤버 초대 보내기 API 서비스 메서드 추가

--- src/main/java/umc/cockple/demo/domain/party/service/PartyCommandService.java
+++ src/main/java/umc/cockple/demo/domain/party/service/PartyCommandService.java
@@ -1,20 +1,16 @@
 package umc.cockple.demo.domain.party.service;
 
 import umc.cockple.demo.domain.party.dto.PartyCreateDTO;
+import umc.cockple.demo.domain.party.dto.PartyInviteCreateDTO;
 import umc.cockple.demo.domain.party.dto.PartyJoinActionDTO;
 import umc.cockple.demo.domain.party.dto.PartyJoinCreateDTO;
 
 public interface PartyCommandService {
-
     PartyCreateDTO.Response createParty(Long memberId, PartyCreateDTO.Request request);
-
     PartyJoinCreateDTO.Response createJoinRequest(Long partyId, Long memberId);
-
     void actionJoinRequest(Long partyId, Long memberId, PartyJoinActionDTO.Request request, Long requestId);
-
     void deleteParty(Long partyId, Long memberId);
-
     void leaveParty(Long partyId, Long memberId);
-
     void removeMember(Long partyId, Long memberIdToRemove, Long currentMemberId);
+    PartyInviteCreateDTO.Response createInvitation(Long partyId, Long memberIdToInvite, Long currentMemberId);
 }
