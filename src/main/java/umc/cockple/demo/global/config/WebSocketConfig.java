0e746ac2cc2c07967ec34c74e5e34cc46cac8b05
dmori <chosehoon2@naver.com>
2025-08-04 01:26:25 +0900

    feat: 웹소켓 핸들러 틀 구현

--- src/main/java/umc/cockple/demo/global/config/WebSocketConfig.java
+++ src/main/java/umc/cockple/demo/global/config/WebSocketConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import umc.cockple.demo.domain.chat.handler.ChatWebSocketHandler;
 
 @Configuration
 @EnableWebSocket
