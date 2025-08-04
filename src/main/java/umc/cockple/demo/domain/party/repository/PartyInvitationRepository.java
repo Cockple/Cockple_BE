f8c4c164b41ec1e61151a243fbd7f95e9f0e21a6
dbalsk <dbalsrltk@naver.com>
2025-08-04 21:56:02 +0900

    feat: 멤버 초대 리포지토리 구현

--- src/main/java/umc/cockple/demo/domain/party/repository/PartyInvitationRepository.java
+++ src/main/java/umc/cockple/demo/domain/party/repository/PartyInvitationRepository.java
@@ -1,4 +1,11 @@
 package umc.cockple.demo.domain.party.repository;
 
-public interface PartyInvitationRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import umc.cockple.demo.domain.member.domain.Member;
+import umc.cockple.demo.domain.party.domain.Party;
+import umc.cockple.demo.domain.party.domain.PartyInvitation;
+import umc.cockple.demo.domain.party.enums.RequestStatus;
+
+public interface PartyInvitationRepository extends JpaRepository<PartyInvitation, Long> {
+    boolean existsByPartyAndInviteeAndStatus(Party party, Member invitee, RequestStatus requestStatus);
 }
