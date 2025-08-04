b277dc17d7d4a5e8df47c499cbc489a67c357680
rim <kkjjrr0504@gmail.com>
2025-08-04 15:17:49 +0900

    feat: 컨버터 구현

--- src/main/java/umc/cockple/demo/domain/chat/converter/ChatConverter.java
+++ src/main/java/umc/cockple/demo/domain/chat/converter/ChatConverter.java
@@ -6,6 +6,7 @@ import umc.cockple.demo.domain.chat.domain.ChatMessage;
 import umc.cockple.demo.domain.chat.domain.ChatRoom;
 import umc.cockple.demo.domain.chat.domain.ChatRoomMember;
 import umc.cockple.demo.domain.chat.dto.DirectChatRoomCreateDTO;
+import umc.cockple.demo.domain.chat.dto.DirectChatRoomDTO;
 import umc.cockple.demo.domain.chat.dto.PartyChatRoomDTO;
 
 import java.util.List;
@@ -15,6 +16,7 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class ChatConverter {
 
+    // ===모임 채팅방 목록===
     public PartyChatRoomDTO.Response toPartyChatRoomListResponse(List<PartyChatRoomDTO.ChatRoomInfo> chatRoomInfos) {
         return PartyChatRoomDTO.Response.builder()
                 .content(chatRoomInfos)
@@ -22,11 +24,11 @@ public class ChatConverter {
                 .build();
     }
 
-    public PartyChatRoomDTO.ChatRoomInfo toChatRoomInfo(ChatRoom chatRoom,
-                                                        int memberCount,
-                                                        int unreadCount,
-                                                        PartyChatRoomDTO.LastMessageInfo lastMessageInfo,
-                                                        String imgUrl) {
+    public PartyChatRoomDTO.ChatRoomInfo toPartyChatRoomInfo(ChatRoom chatRoom,
+                                                             int memberCount,
+                                                             int unreadCount,
+                                                             PartyChatRoomDTO.LastMessageInfo lastMessageInfo,
+                                                             String imgUrl) {
         return PartyChatRoomDTO.ChatRoomInfo.builder()
                 .chatRoomId(chatRoom.getId())
                 .partyId(chatRoom.getId())
@@ -38,7 +40,7 @@ public class ChatConverter {
                 .build();
     }
 
-    public PartyChatRoomDTO.LastMessageInfo toLastMessageInfo(ChatMessage message) {
+    public PartyChatRoomDTO.LastMessageInfo toPartyLastMessageInfo(ChatMessage message) {
         if (message == null) return null;
 
         return PartyChatRoomDTO.LastMessageInfo.builder()
@@ -49,6 +51,40 @@ public class ChatConverter {
                 .build();
     }
 
+    // ===개인 채팅방 목록===
+    public DirectChatRoomDTO.Response toDirectChatRoomListResponse(List<DirectChatRoomDTO.ChatRoomInfo> chatRoomInfos) {
+        return DirectChatRoomDTO.Response.builder()
+                .content(chatRoomInfos)
+                .totalElements(chatRoomInfos.size())
+                .build();
+    }
+
+    public DirectChatRoomDTO.ChatRoomInfo toDirectChatRoomInfo(ChatRoom chatRoom,
+                                                               ChatRoomMember chatRoomMember,
+                                                               int unreadCount,
+                                                               DirectChatRoomDTO.LastMessageInfo lastMessageInfo,
+                                                               String imgUrl) {
+        return DirectChatRoomDTO.ChatRoomInfo.builder()
+                .chatRoomId(chatRoom.getId())
+                .displayName(chatRoomMember.getDisplayName())
+                .profileImgUrl(imgUrl)
+                .unreadCount(unreadCount)
+                .lastMessage(lastMessageInfo)
+                .build();
+    }
+
+    public DirectChatRoomDTO.LastMessageInfo toDirectLastMessageInfo(ChatMessage message) {
+        if (message == null) return null;
+
+        return DirectChatRoomDTO.LastMessageInfo.builder()
+                .messageId(message.getId())
+                .content(message.getContent())
+                .timestamp(message.getCreatedAt())
+                .messageType(message.getType().name())
+                .build();
+    }
+
+    // ===개인 채팅방 생성===
     public DirectChatRoomCreateDTO.Response toDirectChatRoomCreateDTO(ChatRoom chatRoom, List<ChatRoomMember> members, String displayName) {
         return DirectChatRoomCreateDTO.Response.builder()
                 .chatRoomId(chatRoom.getId())
