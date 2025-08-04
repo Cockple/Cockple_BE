476cb13188a888d6a04e3271135f6c0e403dc2cf
dmori <chosehoon2@naver.com>
2025-08-04 13:39:09 +0900

    feat: 메시지 생성 및 저장 로직 구현

--- src/main/java/umc/cockple/demo/domain/chat/service/ChatWebSocketService.java
+++ src/main/java/umc/cockple/demo/domain/chat/service/ChatWebSocketService.java
@@ -4,10 +4,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import umc.cockple.demo.domain.chat.domain.ChatMessage;
 import umc.cockple.demo.domain.chat.domain.ChatRoom;
 import umc.cockple.demo.domain.chat.dto.WebSocketMessageDTO;
+import umc.cockple.demo.domain.chat.enums.MessageType;
 import umc.cockple.demo.domain.chat.exception.ChatErrorCode;
 import umc.cockple.demo.domain.chat.exception.ChatException;
+import umc.cockple.demo.domain.chat.repository.ChatMessageRepository;
 import umc.cockple.demo.domain.chat.repository.ChatRoomRepository;
 import umc.cockple.demo.domain.member.domain.Member;
 import umc.cockple.demo.domain.member.repository.MemberRepository;
@@ -20,6 +23,7 @@ public class ChatWebSocketService {
 
     private final ChatRoomRepository chatRoomRepository;
     private final MemberRepository memberRepository;
+    private final ChatMessageRepository chatMessageRepository;
 
     public WebSocketMessageDTO.Response sendMessage(Long chatRoomId, String content, Long senderId) {
         log.info("메시지 전송 시작 - 채팅방: {}, 발신자: {}", chatRoomId, senderId);
@@ -28,6 +32,12 @@ public class ChatWebSocketService {
         Member sender = findMemberOrThrow(senderId);
 
         validateChatRoomMember(chatRoom, sender);
+
+        // TODO: 다양한 타입의 텍스트 전송가능하도록 변경해야 함
+        ChatMessage chatMessage = ChatMessage.create(chatRoom, sender, content, MessageType.TEXT);
+        ChatMessage savedMessage = chatMessageRepository.save(chatMessage);
+        log.info("메시지 저장 완료 - 메시지 ID: {}", savedMessage.getId());
+
     }
 
     private void validateChatRoomMember(ChatRoom chatRoom, Member member) {
