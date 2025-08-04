b65aa92dda8c8f526b65f299b0ff94dc16be2c15
dmori <chosehoon2@naver.com>
2025-08-04 11:52:43 +0900

    feat: 클라이언트에서 서버로 보내는 Request DTO 구현

--- src/main/java/umc/cockple/demo/domain/chat/dto/WebSocketMessageDTO.java
+++ src/main/java/umc/cockple/demo/domain/chat/dto/WebSocketMessageDTO.java
@@ -1,6 +1,7 @@
 package umc.cockple.demo.domain.chat.dto;
 
 import lombok.Builder;
+import umc.cockple.demo.domain.chat.enums.WebSocketMessageType;
 
 import java.time.LocalDateTime;
 
@@ -13,5 +14,12 @@ public class WebSocketMessageDTO {
             String memberName,
             LocalDateTime connectedAt,
             String message) {
+    }
+
+    public record Request(
+            WebSocketMessageType type,
+            Long chatRoomId,
+            String content
+    ) {
     }
 }
