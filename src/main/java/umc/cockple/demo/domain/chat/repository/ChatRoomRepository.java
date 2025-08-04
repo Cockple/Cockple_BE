06de7cad2aa4c1e8cfab4f1731c9870aa8f5f58b
dmori <chosehoon2@naver.com>
2025-08-04 13:46:31 +0900

    fix: rebase 충돌 해결

--- src/main/java/umc/cockple/demo/domain/chat/repository/ChatRoomRepository.java
+++ src/main/java/umc/cockple/demo/domain/chat/repository/ChatRoomRepository.java
@@ -15,10 +15,9 @@ public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
             SELECT cr FROM ChatRoom cr
             JOIN cr.chatRoomMembers crm
             WHERE crm.member.id = :memberId
-            AND cr.type = 'PARTY'
             AND (:cursor IS NULL OR
-                 (:direction = 'desc' AND cr.id < :cursor) OR
-                 (:direction = 'asc' AND cr.id > :cursor))
+                (:direction = 'desc' AND cr.id < :cursor) OR
+                (:direction = 'asc' AND cr.id > :cursor))
             ORDER BY
                 CASE WHEN :direction = 'asc' THEN cr.id END ASC,
                 CASE WHEN :direction = 'desc' THEN cr.id END DESC
@@ -31,7 +30,6 @@ public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
     );
 
     @Query("""
-            
             SELECT cr FROM ChatRoom cr
             JOIN cr.chatRoomMembers crm
             WHERE crm.member.id = :memberId
