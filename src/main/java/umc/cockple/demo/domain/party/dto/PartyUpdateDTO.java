2ebee07e0fbc61ebe48840f78d292d39267643f2
dbalsk <dbalsrltk@naver.com>
2025-08-06 00:21:50 +0900

    fix: 모임 정보 수정 DTO 검증 추가

--- src/main/java/umc/cockple/demo/domain/party/dto/PartyUpdateDTO.java
+++ src/main/java/umc/cockple/demo/domain/party/dto/PartyUpdateDTO.java
@@ -1,6 +1,8 @@
 package umc.cockple.demo.domain.party.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Builder;
 
 import java.util.List;
@@ -9,7 +11,9 @@ public class PartyUpdateDTO {
     @Builder
     @Schema(name = "PartyUpdateRequestDTO", description = "모임 정보 수정 요청")
     public record Request(
+            @NotEmpty(message = "활동 요일은 필수 선택 항목입니다.")
             List<String> activityDay,
+            @NotBlank(message = "활동 시간은 필수 선택 항목입니다.")
             String activityTime,
             String designatedCock,
             Integer joinPrice,
