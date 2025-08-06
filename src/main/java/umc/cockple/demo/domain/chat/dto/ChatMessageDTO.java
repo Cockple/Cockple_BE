bf411e97b7605d59a80dff04f7bd46370c36d977
dmori <chosehoon2@naver.com>
2025-08-06 22:53:03 +0900

    feat: DTO 구현

---
+++ src/main/java/umc/cockple/demo/domain/chat/dto/ChatMessageDTO.java
@@ -0,0 +1,30 @@
+package umc.cockple.demo.domain.chat.dto;
+
+import lombok.Builder;
+import umc.cockple.demo.domain.chat.enums.MessageType;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class ChatMessageDTO {
+
+    @Builder
+    public record Response(
+            List<MessageInfo> messages
+    ) {
+    }
+
+    @Builder
+    public record MessageInfo(
+            Long messageId,
+            Long senderId,
+            String senderName,
+            String senderProfileImageUrl,
+            String content,
+            MessageType messageType,
+            List<String> imageUrls,
+            LocalDateTime timestamp,
+            boolean isMyMessage
+    ) {
+    }
+}
