e2f4a4968e1849571ab5f0e33b296f17d39dc00e
dmori <chosehoon2@naver.com>
2025-08-08 21:03:24 +0900

    feat: 모임 입출입 이벤트 리스너 구현

---
+++ src/main/java/umc/cockple/demo/domain/chat/event/ChatEventListener.java
@@ -0,0 +1,27 @@
+package umc.cockple.demo.domain.chat.event;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+import umc.cockple.demo.domain.chat.service.ChatWebSocketService;
+import umc.cockple.demo.domain.party.event.PartyMemberJoinedEvent;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class ChatEventListener {
+
+    private final ChatWebSocketService chatWebSocketService;
+
+    @EventListener
+    @Async
+    public void handlePartyMemberJoined(PartyMemberJoinedEvent event) {
+        String message = switch(event.action()){
+            case JOINED -> event.memberName() + "님이 모임에 참여했습니다.";
+            case LEFT -> event.memberName() + "님이 모임을 떠났습니다.";
+        };
+    }
+
+}
