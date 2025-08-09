4b8d8ea76810811a29725c344116cbfe1e919f34
dmori <chosehoon2@naver.com>
2025-08-10 00:45:48 +0900

    feat: HandShakeInterceptor 구현체 틀 구현

---
+++ src/main/java/umc/cockple/demo/domain/chat/interceptor/JWTWebSocketAuthInterceptor.java
@@ -0,0 +1,28 @@
+package umc.cockple.demo.domain.chat.interceptor;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+
+import java.util.Map;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class JWTWebSocketAuthInterceptor implements HandshakeInterceptor {
+
+    @Override
+    public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response,
+                                   WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
+        return false;
+    }
+
+    @Override
+    public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Exception exception) {
+
+    }
+}
