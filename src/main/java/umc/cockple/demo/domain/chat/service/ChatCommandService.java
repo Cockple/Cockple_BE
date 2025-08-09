9460006c30ad4c97941d28b913fe9bf20aa5faac
dbalsk <dbalsrltk@naver.com>
2025-08-09 16:21:37 +0900

    feat: 다운로드 토큰 발급 API 서비스 메서드 추가

--- src/main/java/umc/cockple/demo/domain/chat/service/ChatCommandService.java
+++ src/main/java/umc/cockple/demo/domain/chat/service/ChatCommandService.java
@@ -1,8 +1,10 @@
 package umc.cockple.demo.domain.chat.service;
 
+import umc.cockple.demo.domain.chat.dto.ChatDownloadTokenDTO;
 import umc.cockple.demo.domain.chat.dto.DirectChatRoomCreateDTO;
 
 public interface ChatCommandService {
 
     DirectChatRoomCreateDTO.Response createDirectChatRoom(Long memberId, Long targetMemberId);
+    ChatDownloadTokenDTO.Response issueDownloadToken(Long memberId, Long fileId);
 }
