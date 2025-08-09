26ff0bf3c2b387b8a61fb312118ca205121eef10
dbalsk <dbalsrltk@naver.com>
2025-08-09 23:14:40 +0900

    feat: 채팅 파일 다운로드 API 리포지토리 메서드 추가

--- src/main/java/umc/cockple/demo/domain/chat/repository/DownloadTokenRepository.java
+++ src/main/java/umc/cockple/demo/domain/chat/repository/DownloadTokenRepository.java
@@ -3,5 +3,8 @@ package umc.cockple.demo.domain.chat.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import umc.cockple.demo.domain.chat.domain.DownloadToken;
 
+import java.util.Optional;
+
 public interface DownloadTokenRepository extends JpaRepository<DownloadToken, Long> {
+    Optional<DownloadToken> findByToken(String token);
 }
