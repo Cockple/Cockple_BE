fb614c4829f6f5737f8cad938a75dc0b73bb77ac
dmori <chosehoon2@naver.com>
2025-08-10 01:09:40 +0900

    feat: 멤버 id를 jwt에서 추출하여 세션에 넣어두기

--- src/main/java/umc/cockple/demo/domain/chat/interceptor/JWTWebSocketAuthInterceptor.java
+++ src/main/java/umc/cockple/demo/domain/chat/interceptor/JWTWebSocketAuthInterceptor.java
@@ -4,9 +4,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.server.HandshakeInterceptor;
+import umc.cockple.demo.global.jwt.domain.JwtTokenProvider;
 
 import java.util.Map;
 
@@ -14,6 +16,8 @@ import java.util.Map;
 @Slf4j
 @RequiredArgsConstructor
 public class JWTWebSocketAuthInterceptor implements HandshakeInterceptor {
+
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Override
     public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response,
@@ -23,10 +27,26 @@ public class JWTWebSocketAuthInterceptor implements HandshakeInterceptor {
         try {
             String token = extractTokenFromRequest(request);
 
+            if (isValidToken(token)) return false;
+
+            Long memberId = jwtTokenProvider.getUserId(token);
+
+            attributes.put("memberId", memberId);
+            attributes.put("authenticated", true);
+
+            log.info("JWT 인증 성공 - memberId: {}", memberId);
             return true;
         } catch (Exception e) {
             log.error("JWT 인증 처리 중 오류 발생", e);
             return false;
+        }
+    }
+
+    @Override
+    public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response,
+                               WebSocketHandler wsHandler, Exception exception) {
+        if (exception != null) {
+            log.error("JWT HandshakeInterceptor 실행 중 오류", exception);
         }
     }
 
@@ -45,11 +65,16 @@ public class JWTWebSocketAuthInterceptor implements HandshakeInterceptor {
         return null;
     }
 
-    @Override
-    public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response,
-                               WebSocketHandler wsHandler, Exception exception) {
-        if (exception != null) {
-            log.error("JWT HandshakeInterceptor 실행 중 오류", exception);
+    private boolean isValidToken(String token) {
+        if (token == null) {
+            log.error("JWT 토큰이 없습니다");
+            return true;
         }
+
+        if (!jwtTokenProvider.validateToken(token)) {
+            log.error("유효하지 않은 JWT 토큰");
+            return true;
+        }
+        return false;
     }
 }
