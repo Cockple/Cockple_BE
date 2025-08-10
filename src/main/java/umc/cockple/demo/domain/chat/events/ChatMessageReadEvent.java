1888b4cb09846fc8c4e56fc94c64285e0985589f
dmori <chosehoon2@naver.com>
2025-08-10 17:49:50 +0900

    feat: 채팅 메시지 읽기 이벤트 구현

--- src/main/java/umc/cockple/demo/domain/chat/events/ChatMessageReadEvent.java
+++ src/main/java/umc/cockple/demo/domain/chat/events/ChatMessageReadEvent.java
@@ -1,4 +1,18 @@
 package umc.cockple.demo.domain.chat.events;
 
-public record ChatMessageReadEvent() {
+import lombok.Builder;
+
+@Builder
+public record ChatMessageReadEvent(
+        Long chatRoomId,
+        Long lastReadMessageId,
+        Long readerId
+) {
+    public static ChatMessageReadEvent create(Long chatRoomId, Long lastReadMessageId, Long readerId) {
+        return ChatMessageReadEvent.builder()
+                .chatRoomId(chatRoomId)
+                .lastReadMessageId(lastReadMessageId)
+                .readerId(readerId)
+                .build();
+    }
 }
