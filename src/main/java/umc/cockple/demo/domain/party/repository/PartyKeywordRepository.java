4c100a3011c51c7ec4c61c1257b72fefe3fad581
dbalsk <dbalsrltk@naver.com>
2025-08-12 23:42:25 +0900

    feat: 키워드 추가 API 리포지토리 구현

--- src/main/java/umc/cockple/demo/domain/party/repository/PartyKeywordRepository.java
+++ src/main/java/umc/cockple/demo/domain/party/repository/PartyKeywordRepository.java
@@ -1,4 +1,10 @@
 package umc.cockple.demo.domain.party.repository;
 
-public interface PartyKeywordRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import umc.cockple.demo.domain.party.domain.Party;
+import umc.cockple.demo.domain.party.domain.PartyKeyword;
+import umc.cockple.demo.global.enums.Keyword;
+
+public interface PartyKeywordRepository extends JpaRepository<PartyKeyword, Long> {
+    boolean existsByPartyAndKeyword(Party party, Keyword keyword);
 }
