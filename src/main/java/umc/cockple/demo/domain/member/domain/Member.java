a38819038593ae882dc32a6644c87732332f44ba
강하나 <107329874+kanghana1@users.noreply.github.com>
2025-08-12 21:51:38 +0900

    [feat/#322] 탈퇴 회원 시큐리티 필터에서 검증 (#327)
    * feat: 탈퇴 회원 시큐리티 필터에서 검증
    
    * fix: 에러메시지  수정

--- src/main/java/umc/cockple/demo/domain/member/domain/Member.java
+++ src/main/java/umc/cockple/demo/domain/member/domain/Member.java
@@ -171,4 +171,8 @@ public class Member extends BaseEntity {
     public void setRefreshToken(String refreshToken) {
         this.refreshToken = refreshToken;
     }
+
+    public void rejoin() {
+        this.isActive = MemberStatus.ACTIVE;
+    }
 }
