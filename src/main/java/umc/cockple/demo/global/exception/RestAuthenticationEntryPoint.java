d9d124f42e1dff7859920ef6801eb4f74edca282
kanghana1 <qazplm12093@naver.com>
2025-08-12 16:26:11 +0900

    fix: 에러메시지  수정

--- src/main/java/umc/cockple/demo/global/exception/RestAuthenticationEntryPoint.java
+++ src/main/java/umc/cockple/demo/global/exception/RestAuthenticationEntryPoint.java
@@ -16,8 +16,8 @@ public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED); // 401
         response.setContentType("application/json;charset=UTF-8");
         String body = """
-        {"success":false,"code":"AUTH-401","message":"%s"}
-        """.formatted(authException.getMessage() == null ? "Unauthorized" : authException.getMessage());
+        {"success":false,"code":"AUTH401","message":"%s"}
+        """.formatted(authException.getMessage() == null ? "권한이 존재하지 않습니다" : authException.getMessage());
         response.getWriter().write(body);
     }
 }
