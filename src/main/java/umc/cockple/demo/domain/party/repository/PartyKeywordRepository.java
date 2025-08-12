eb8ff8d651d80f49219cd46329bfd01926820a09
dbalsk <dbalsrltk@naver.com>
2025-08-13 00:30:54 +0900

    feat: 키워드 삭제 API 리포지토리 구현

--- src/main/java/umc/cockple/demo/domain/party/repository/PartyKeywordRepository.java
+++ src/main/java/umc/cockple/demo/domain/party/repository/PartyKeywordRepository.java
@@ -5,6 +5,9 @@ import umc.cockple.demo.domain.party.domain.Party;
 import umc.cockple.demo.domain.party.domain.PartyKeyword;
 import umc.cockple.demo.global.enums.Keyword;
 
+import java.util.Optional;
+
 public interface PartyKeywordRepository extends JpaRepository<PartyKeyword, Long> {
     boolean existsByPartyAndKeyword(Party party, Keyword keyword);
+    Optional<PartyKeyword> findByPartyAndKeyword(Party party, Keyword keyword);
 }
