d877cede6a16cc9492c0aaf51ae170cbad05e242
dmori <chosehoon2@naver.com>
2025-08-14 21:23:42 +0900

    feat: 브로드캐스트 할 DTO에 안 읽은 멤버 필드 추가

--- src/main/java/umc/cockple/demo/domain/chat/dto/WebSocketMessageDTO.java
+++ src/main/java/umc/cockple/demo/domain/chat/dto/WebSocketMessageDTO.java
@@ -34,7 +34,8 @@ public class WebSocketMessageDTO {
             Long senderId,
             String senderName,
             String senderProfileImageUrl,
-            LocalDateTime timestamp
+            LocalDateTime timestamp,
+            Integer unreadCount
     ) {
     }
 
