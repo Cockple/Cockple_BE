aca594eaad9f1f73262673188870bfa036860943
dbalsk <dbalsrltk@naver.com>
2025-08-18 19:01:58 +0900

    fix: 키워드 추가 DTO 리스트로 수정

--- src/main/java/umc/cockple/demo/domain/party/dto/PartyKeywordDTO.java
+++ src/main/java/umc/cockple/demo/domain/party/dto/PartyKeywordDTO.java
@@ -2,13 +2,16 @@ package umc.cockple.demo.domain.party.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
+
+import java.util.List;
 
 public class PartyKeywordDTO {
     @Builder
-    @Schema(name = "PartyKeywordRequestDTO", description = "모임 키워드 추가/삭제 요청")
+    @Schema(name = "PartyKeywordRequestDTO", description = "모임 키워드 추가 요청")
     public record Request(
-            @NotBlank(message = "키워드는 필수입니다.")
-            String keyword
+            @NotNull(message = "키워드 목록은 필수입니다.")
+            List<String> keywords
     ){}
 }
