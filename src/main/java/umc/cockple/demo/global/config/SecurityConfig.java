79e8336e0a3ccc3ad0efe21a76bac3c1f2ae5009
dmori <chosehoon2@naver.com>
2026-02-25 12:51:56 +0900

    chore: 필요없는 import문 제거

--- src/main/java/umc/cockple/demo/global/config/SecurityConfig.java
+++ src/main/java/umc/cockple/demo/global/config/SecurityConfig.java
@@ -13,9 +13,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import umc.cockple.demo.domain.member.repository.MemberRepository;
 import umc.cockple.demo.global.exception.RestAuthenticationEntryPoint;
-import umc.cockple.demo.global.jwt.domain.JwtTokenProvider;
 import umc.cockple.demo.global.security.filter.JwtAuthenticationFilter;
 
 import java.util.List;
