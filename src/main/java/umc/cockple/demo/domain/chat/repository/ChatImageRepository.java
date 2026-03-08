d2936099241c1ff4eab0dc165b3d10b909612e17
dmori <chosehoon2@naver.com>
2026-03-08 14:07:18 +0900

    chore: 사용하지 않는 ChatImageRepository 제거

--- src/main/java/umc/cockple/demo/domain/chat/repository/ChatImageRepository.java
+++
@@ -1,7 +0,0 @@
-package umc.cockple.demo.domain.chat.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import umc.cockple.demo.domain.chat.domain.ChatMessageFile;
-
-public interface ChatImageRepository extends JpaRepository<ChatMessageFile, Long> {
-}
