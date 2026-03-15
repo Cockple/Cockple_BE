9c0b059d0f4bbfa725919fe3803d2f0e7e99a38d
kanghana1 <qazplm12093@naver.com>
2026-03-15 16:57:14 +0900

    test: 통합테스트 config에 mock 추가

--- src/main/java/umc/cockple/demo/global/config/FirebaseConfig.java
+++ src/main/java/umc/cockple/demo/global/config/FirebaseConfig.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -18,6 +19,7 @@ import umc.cockple.demo.global.exception.GlobalExceptionHandler;
 
 @Slf4j
 @Configuration
+@Profile("!integrationtest")
 public class FirebaseConfig {
 
     @Value("${firebase.key-path}")
