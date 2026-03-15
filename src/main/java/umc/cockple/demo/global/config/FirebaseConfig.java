789eaf6dc4f635bfa82b4db66955ac70dc1ecd66
kanghana1 <qazplm12093@naver.com>
2026-03-15 15:04:42 +0900

    feat: firebaseConfig파일 생성

---
+++ src/main/java/umc/cockple/demo/global/config/FirebaseConfig.java
@@ -0,0 +1,40 @@
+package umc.cockple.demo.global.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+@Slf4j
+@Configuration
+public class FirebaseConfig {
+
+    @Bean
+    public FirebaseApp firebaseApp() throws IOException {
+        if (!FirebaseApp.getApps().isEmpty()) {
+            return FirebaseApp.getInstance();
+        }
+
+        InputStream serviceAccount = new ClassPathResource(
+                "firebase/cockple-1a83e-firebase-adminsdk-fbsvc-212ce01565.json"
+        ).getInputStream();
+
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+        return FirebaseApp.initializeApp(options);
+    }
+
+    @Bean
+    public FirebaseMessaging firebaseMessaging(FirebaseApp firebaseApp) {
+        return FirebaseMessaging.getInstance(firebaseApp);
+    }
+}
