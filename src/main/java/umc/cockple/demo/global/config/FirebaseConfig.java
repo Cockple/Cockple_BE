c674263659252c6c5d3b785f224a041ec76ee134
kanghana1 <qazplm12093@naver.com>
2026-03-15 15:17:32 +0900

    feat: 설정파일 추가

--- src/main/java/umc/cockple/demo/global/config/FirebaseConfig.java
+++ src/main/java/umc/cockple/demo/global/config/FirebaseConfig.java
@@ -5,10 +5,11 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -16,15 +17,16 @@ import java.io.InputStream;
 @Configuration
 public class FirebaseConfig {
 
+    @Value("${firebase.key-path}")
+    private String keyPath;
+
     @Bean
     public FirebaseApp firebaseApp() throws IOException {
         if (!FirebaseApp.getApps().isEmpty()) {
             return FirebaseApp.getInstance();
         }
 
-        InputStream serviceAccount = new ClassPathResource(
-                "firebase/cockple-1a83e-firebase-adminsdk-fbsvc-212ce01565.json"
-        ).getInputStream();
+        InputStream serviceAccount = new FileInputStream(keyPath);
 
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
