f730c4d0d802920ca7c21b3490a386c4dea65313
Dimo-2562 <chosehoon2@naver.com>
2026-04-12 20:39:27 +0900

    debug: 통합 테스트에서 redis를 ServiceConnection이 아닌 RedisConfig 값을 사용하던 문제를 해결

--- src/test/java/umc/cockple/demo/support/IntegrationTestConfig.java
+++ src/test/java/umc/cockple/demo/support/IntegrationTestConfig.java
@@ -5,6 +5,10 @@ import com.redis.testcontainers.RedisContainer;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
@@ -35,6 +39,14 @@ public class IntegrationTestConfig {
     @ServiceConnection
     RedisContainer redisContainer() {
         return redis;
+    }
+
+    @Bean
+    @Primary
+    RedisConnectionFactory testRedisConnectionFactory(RedisContainer redisContainer) {
+        RedisStandaloneConfiguration configuration =
+                new RedisStandaloneConfiguration(redisContainer.getHost(), redisContainer.getMappedPort(6379));
+        return new LettuceConnectionFactory(configuration);
     }
 
     @Bean
